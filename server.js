4578749c7031e6c09af3945a91288fb2afe6e102
nmkhedekar <nmkhedekar@virtualemployee.com>
2023-03-09 17:25:07 +0530

    updated

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const cors = require("cors");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/gecko").then(() => {
+    console.log("Database connected...");
+});
 
 app.get("/", (req, res) => {
     res.send("listening on port 5000");
